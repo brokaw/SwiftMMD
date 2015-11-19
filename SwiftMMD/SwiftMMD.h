c524e71cfc81580f9103fc7fd3fe314790f249e9
Steve Brokaw <brokaw@twocanoes.com>
2015-11-18 19:17:14 -0600

    Initial Commit

---
+++ SwiftMMD/SwiftMMD.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMMD.h
+//  SwiftMMD
+//
+//  Created by Steve Brokaw on 11/18/15.
+//  Copyright © 2015 Steven Brokaw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftMMD.
+FOUNDATION_EXPORT double SwiftMMDVersionNumber;
+
+//! Project version string for SwiftMMD.
+FOUNDATION_EXPORT const unsigned char SwiftMMDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMMD/PublicHeader.h>
+
+
