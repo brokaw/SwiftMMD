e00efd6cdca57b62acbbe3f9c22a7e9947c2cf34
Steve Brokaw <brokaw@me.com>
2015-11-23 17:04:54 -0600

    Updated to use String.cStringToString()

--- SwiftMMD/MultiMarkdown.swift
+++ SwiftMMD/MultiMarkdown.swift
@@ -8,52 +8,61 @@
 
 import Foundation
 
-struct MMDExtensions: OptionSetType {
-    let rawValue: UInt
-    init(rawValue: UInt) {
+public struct ParserExtension: OptionSetType {
+    public let rawValue: UInt
+    public init(rawValue: UInt) {
         self.rawValue = rawValue
     }
-    static let Compatibility = MMDExtensions(rawValue: UInt(EXT_COMPATIBILITY.rawValue))
-    static let Complete = MMDExtensions(rawValue: UInt(EXT_COMPLETE.rawValue))
+    public static let Compatibility = ParserExtension(rawValue: UInt(EXT_COMPATIBILITY.rawValue))
+    public static let Complete = ParserExtension(rawValue: UInt(EXT_COMPLETE.rawValue))
     //etc
 }
 
-public func mmd_string_to_markdown(string: String) -> String {
+public enum ExportFormat: UInt32 {
+    case HTML
+}
+public func mmd_string_to_markdown(string: String) -> String? {
     let output = markdown_to_string(string, UInt(EXT_COMPLETE.rawValue), Int32(HTML_FORMAT.rawValue))
-    return String.cStringToString(output)
+    let str = String.fromCString(output)
+    free(output)
+    return str
 }
 
 public extension String {
 
-    public func mmdToHTMLDocument() -> String {
+    public func mmdToHTMLDocument() -> String? {
         let output = markdown_to_string(self, UInt(EXT_COMPLETE.rawValue), Int32(HTML_FORMAT.rawValue))
-        return String.cStringToString(output)
+        let str = String.fromCString(output)
+        free(output)
+        return str
     }
-    public func mmdRenderToFormat(format: export_formats, extensions: parser_extensions) -> String {
+    public func mmdRenderToFormat(format: export_formats, extensions: parser_extensions) -> String? {
         let output = markdown_to_string(self, UInt(extensions.rawValue), Int32(format.rawValue))
-        return String.cStringToString(output)
+        let str = String.fromCString(output)
+        free(output)
+        return str
+    }
+    public func mmdRenderToFormat(format: ExportFormat, extensions: ParserExtension) -> String? {
+        let format: export_formats = export_formats(format.rawValue)
+        let ext: parser_extensions = parser_extensions(1)
+        self.mmdRenderToFormat(format, extensions: extensions)
     }
     public func mmdHasMetadataWithExtenstion(extensions: parser_extensions) -> Bool {
         return has_metadata(self, UInt(extensions.rawValue))
     }
-    public func mmdMetadataKeysWithExtensions(extensions: parser_extensions) -> String {
+    public func mmdMetadataKeysWithExtensions(extensions: parser_extensions) -> String? {
         let output = extract_metadata_keys(self, UInt(extensions.rawValue))
-        return String.cStringToString(output)
-    }
-
-    public func mmdMetadataValueForKey(key:String, extensions: parser_extensions) -> String {
-        let keyData = key.dataUsingEncoding(NSUTF8StringEncoding)!
-        let keyPtr = UnsafeMutablePointer<Int8>(keyData.bytes)
-        let output = extract_metadata_value(self, UInt(extensions.rawValue), keyPtr)
-        return String.cStringToString(output)
-    }
-
-    static private func cStringToString(cString: UnsafeMutablePointer<Int8>) -> String {
-        let len = Int(strlen(cString))
-        let data = NSData(bytes: cString, length: len)
-        let str = String(data: data, encoding: NSUTF8StringEncoding)!
-        free(cString)
+        let str = String.fromCString(output)
+        free(output)
         return str
     }
 
+    public func mmdMetadataValueForKey(key:String, extensions: parser_extensions) -> String? {
+        let keyData = key.dataUsingEncoding(NSUTF8StringEncoding)!
+        let keyPtr = UnsafeMutablePointer<Int8>(keyData.bytes)
+        let output = extract_metadata_value(self, UInt(extensions.rawValue), keyPtr)
+        let str = String.fromCString(output)
+        free(output)
+        return str
+    }
 }
